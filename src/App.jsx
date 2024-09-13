f53a9a184543a82e50ad1a6f9a4b8827a9aa35ee
Thangga7 <beencules@gmail.com>
2024-09-14 02:00:51 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./components/Layouts/Home/home";
+
+function App() {
+  return (
+    <div className="">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
